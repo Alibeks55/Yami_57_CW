fd1fb51bca6e820facee803429031c43a71ff487
Alibek Niyazbekov <alibeksharsheev@gmail.com>
2026-06-10 23:40:47 +0600

    2 классная сделанаа

--- cinema_afisha/films/models.py
+++ cinema_afisha/films/models.py
@@ -2,8 +2,24 @@ from django.db import models
 
 
 
+class Director(models.Model):
+    fio = models.CharField(max_length=255)
+    birthday = models.DateField()
+
+    def __str__(self):
+        return self.fio
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Film(models.Model):
+    director = models.ForeignKey(Director, on_delete=models.CASCADE, null=True)
+    genres = models.ManyToManyField(Genre, blank=True)
     title = models.CharField(max_length=255)
     text = models.TextField(blank=True, null=True)
     release_year = models.IntegerField()
@@ -12,5 +28,22 @@ class Film(models.Model):
     created = models.DateField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    def __str___(self):
+    def __str__(self):
         return self.title
+
+    @property
+    def gener_list(self):
+        return [i.name for i in self.genres.all()]
+
+STARS = (
+   (i, '⭐️' * i) for i in range(1,11)
+)
+
+
+class Review(models.Model):
+    text = models.TextField()
+    stars = models.IntegerField(choices=STARS, default=7)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE,
+                             related_name='reviews')
+    def __str__(self):
+        return self.text
--- cinema_afisha/films/serializers.py
+++ cinema_afisha/films/serializers.py
@@ -1,11 +1,32 @@
 from rest_framework import serializers
-from .models import Film
+from .models import Film, Director, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['id', 'text', 'stars']
+
+
+class DirectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Director
+        fields = ['id', 'fio', 'birthday' ]
 
 
 class FilmListSerializer(serializers.ModelSerializer):
+    director = DirectorSerializer(many=False)
+    genres = serializers.SerializerMethodField()
+    reviews = ReviewSerializer(many=True)
+
     class Meta:
         model = Film
-        fields = ['id', 'title', 'release_year', 'rating', 'is_hit']
+        fields = ['id', 'title', 'release_year', 'rating', 'is_hit', 'director', 'genres', 'reviews']
+        #depth = 1
+
+    def get_genres(self, film):
+         return film.gener_list
+
 
 class FilmDetailSerializer(serializers.ModelSerializer):
     class Meta:
